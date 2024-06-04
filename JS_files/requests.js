5c2815d79b6aa9e05704141ba81c0b4e327632c4
Abega1642 <hei.abega.2@gmail.com>
2024-06-04 14:19:46 +0330

    feat: deleting useless lines

--- JS_files/requests.js
+++ JS_files/requests.js
@@ -13,13 +13,4 @@ pool.connect(function (err, client, done) {
     client.query('SELECT * FROM "book"', (err, res) => {
         console.table(res["rows"])
     })
-})
-
-
-    echo "# Base de Données -- Bibliothèque" >> README.md
-    git init
-    git add README.md
-    git commit -m "feat: creating readme.md"
-    git branch -M main
-    git remote add origin https://github.com/Abega1642/BD-Biblioth-que.git
-    git push -u origin main
+});
