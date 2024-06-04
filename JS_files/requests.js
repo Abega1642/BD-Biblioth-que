5339dadb9980b8b5e05b7bba517f2704e11180f6
Abega1642 <hei.abega.2@gmail.com>
2024-06-04 15:22:01 +0330

    refactor: rename a function

--- JS_files/requests.js
+++ JS_files/requests.js
@@ -7,25 +7,21 @@ let config = {
     port: 5432,
 };
 
-
 let pool = new pg.Pool(config);
+
 function availableBooks() {
     pool.connect(function (err, client) {
         client.query('SELECT title, number_of_pages, release_date, "status", "language" FROM book', (err, res) => {
-            console.table(res["rows"])
+            console.table(res["rows"]);
         })
     });
 };
 
-function booksBelongstoASpesificAuthor(authorName) {
+function booksAndAuthor(authorName) {
     pool.connect(function (err, client) {
-        client.query('SELECT title, number_of_pages, release_date, "status", "language" FROM written_by INNER JOIN book ON book.id_book = written_by.id_book INNER JOIN author ON author.id_author = written_by.id_author WHERE author.last_name = "George"', (res, err) => {
-            console.table(res["rows"])
+        client.query(`SELECT title, number_of_pages, release_date, "status", "language" FROM book INNER JOIN written_by ON book.id_book = written_by.id_book INNER JOIN author ON author.id_author = written_by.id_author WHERE author.last_name = ${authorName}`, (res, err) => {
+            console.table(res["rows"]);
+            console.log(authorName);
         })
-        if(err) {
-            console.error(err.message);
-        }
     })
-}
-
-booksBelongstoASpesificAuthor()
+};
--- SQL_files/requests.sql
+++ SQL_files/requests.sql
@@ -4,7 +4,7 @@ SELECT
     release_date, 
     "status", 
     "language" 
-FROM book
+FROM book;
 
 SELECT 
     title,
@@ -16,4 +16,4 @@ FROM book INNER JOIN written_by
     ON book.id_book = written_by.id_book
 INNER JOIN author
     ON author.id_author = written_by.id_author
-WHERE author.last_name = 'George';
+WHERE author.last_name = '...';
