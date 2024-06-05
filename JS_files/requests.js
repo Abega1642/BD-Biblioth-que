13cebecb0f3db408b43dd0b877798c6b9326afa8
Abega1642 <hei.abega.2@gmail.com>
2024-06-05 16:07:49 +0330

    feat: adding new request & code identation

--- JS_files/requests.js
+++ JS_files/requests.js
@@ -1,17 +1,47 @@
-const pg = require('pg')
+import pkg from 'pg';
+const { Pool } = pkg;
+
 
 let config = {
     user: 'postgres',
-    database : 'gestion_bibliotheque',
+    database: 'gestion_bibliotheque',
     password: 'razafindratelo',
     port: 5432,
 };
 
-let pool = new pg.Pool(config);
+const pool = new Pool(config);
 
 function searchingBookByTitle(BookTitle) {
     pool.connect(function (err, client) {
-        client.query(`SELECT title, number_of_pages, release_date, "status", "language" FROM book WHERE title ilike "%$1%`, [BookTitle], (err, res) => {
+        client.query(`
+        SELECT
+            title, 
+            number_of_pages, 
+            release_date, 
+            "status", 
+            "language" 
+            FROM book 
+            WHERE title ilike "%$1%`, [BookTitle], (err, res) => {
+            console.table(res["rows"]);
+        })
+    });
+};
+
+function searchingBookByGenre() {
+    pool.connect(function (err, client) {
+        client.query(`
+        SELECT
+            genre_name
+            title,
+            number_of_pages,
+            release_date,
+            "status",
+            "language"
+        FROM book INNER JOIN belong 
+            ON book.id_book = belong.id_book
+        INNER JOIN genre
+        ON genre.id_genre = belong.id_genre
+        GROUP BY genre_name;`, (err, res) => {
             console.table(res["rows"]);
         })
     });
@@ -23,8 +53,17 @@ function booksAndAuthor(authorName) {
             console.error('Erreur de connexion à la base de données :', err);
             return;
         }
-
-        client.query(`SELECT title, number_of_pages, release_date, "status", "language" FROM book INNER JOIN written_by ON book.id_book = written_by.id_book INNER JOIN author ON author.id_author = written_by.id_author WHERE author.last_name = $1`, [authorName], (err, res) => {
+        client.query(`
+        SELECT 
+            title, 
+            number_of_pages, 
+            release_date, 
+            "status", "language" 
+        FROM book INNER JOIN written_by 
+            ON book.id_book = written_by.id_book 
+        INNER JOIN author 
+            ON author.id_author = written_by.id_author 
+        WHERE author.last_name = $1`, [authorName], (err, res) => {
             if (err) {
                 console.error('Erreur lors de l\'exécution de la requête SQL :', err);
                 return;
--- SQL_files/requests.sql
+++ SQL_files/requests.sql
@@ -18,5 +18,18 @@ INNER JOIN author
     ON author.id_author = written_by.id_author
 WHERE author.last_name = '...';
 
+SELECT
+    genre_name
+    title,
+    number_of_pages,
+    release_date,
+    "status",
+    "language"
+FROM book INNER JOIN belong 
+    ON book.id_book = belong.id_book
+INNER JOIN genre
+    ON genre.id_genre = belong.id_genre
+GROUP BY genre_name;
+
 select * from author where not exists
 (select * from written_by where author.id_author = written_by.id_author);
