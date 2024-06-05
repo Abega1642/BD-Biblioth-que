2805e9dee82556519e8ce923755b09b2a986f8a8
Abega1642 <hei.abega.2@gmail.com>
2024-06-05 17:39:10 +0330

    feat: adding some changes

--- SQL_files/requests.sql
+++ SQL_files/requests.sql
@@ -31,5 +31,33 @@ INNER JOIN genre
     ON genre.id_genre = belong.id_genre
 GROUP BY genre_name;
 
+SELECT 
+    id_member,
+    last_name,
+    first_name,
+    phone_number,
+    email,
+    end_date,
+    title AS Book_Title
+FROM book INNER JOIN borrow
+    ON book.id_book = borrow.id_book
+INNER JOIN ON member
+    ON member.id_member = borrow.id_member
+
+SELECT 
+    id_member,
+    last_name,
+    first_name,
+    phone_number,
+    email,
+    end_date,
+    title AS Book_Title
+FROM book INNER JOIN borrow
+    ON book.id_book = borrow.id_book
+INNER JOIN ON member
+    ON member.id_member = borrow.id_member
+WHERE end_date = current_date;
+
+
 select * from author where not exists
 (select * from written_by where author.id_author = written_by.id_author);
